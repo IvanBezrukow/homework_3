c29bfa809cbd3b4a8a6d48ec7bf850b81856f2ac
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-05-07 02:19:45 +0300

    completed the fifth task

---
+++ task_5/Program.cs
@@ -0,0 +1,52 @@
+﻿string IfPalindrome(double arg1)
+{
+    string result = "This number is ";
+    double temp = arg1;
+    double remainder = 0;
+    double remainder1 = 0;
+    double tempremainder = 0;
+    double tempremainder1 = 0;
+    double count = 0;
+    double tempcount = 1;
+    while (arg1 > 10)
+    {
+        arg1 = arg1 / 10;
+        count++;
+    }
+    arg1 = Math.Round(arg1 * Math.Pow(10, count));
+    while (count > 0)
+    {
+        tempremainder = Math.Round(arg1 % Math.Pow(10, tempcount));
+        remainder = Math.Floor(arg1 / Math.Pow(10, count));
+        if (tempremainder == remainder)
+        {
+            arg1 = arg1 - (Math.Pow(10, count) * tempremainder);
+            arg1 = Math.Floor(arg1 / 10);
+            tempcount = tempcount++;
+            count = count - 2;
+            tempremainder1 = Math.Round(arg1 % Math.Pow(10, tempcount));
+            remainder1 = Math.Floor(arg1 / Math.Pow(10, count));
+            if (tempremainder1 == remainder1 && arg1 > 9)
+                continue;
+            else
+                return result + "a palindrome";
+        }
+        else
+            return result + "not a palindrome";
+    }
+    return result + "not a palindrome";
+}
+
+
+
+
+try
+{
+    Console.Write("Insert any number = ");
+    double num = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(IfPalindrome(num));
+}
+catch
+{
+    Console.WriteLine("You may insert only numbers");
+}
